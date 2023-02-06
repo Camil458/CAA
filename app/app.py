ad3a872daea86a1177188a2c178c0392d66603ea
Kamil458 <k.radlowski458@gmail.com>
2023-02-06 23:46:49 +0100

    Database connection

--- app/app.py
+++ app/app.py
@@ -8,8 +8,8 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'secret-key-goes-here'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config.from_pyfile('../config.py')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{app.config['DB_USER']}:{app.config['DB_PASSWORD']}@{app.config['DB_HOST']}/{app.config['DB_NAME']}"
 
     db.init_app(app)
 
